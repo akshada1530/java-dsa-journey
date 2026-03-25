e47b8a6c8782752348e3cd83d333489c5dee8a3b
Akshada <akshadamahajan05@gmail.com>
2026-03-25 23:58:29 +0530

    Day 2 - variables and data types in java

---
+++ Month1-java-basics/Day2_Variables.java
@@ -0,0 +1,13 @@
+public class Day2_Variables {
+    public static void main(String[] args) {
+        int age = 20;
+        float marks = 85.5f;
+        String name = "Akshada";
+        boolean isPlacementReady = true;
+        
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Marks: " + marks);
+        System.out.println("Placement Ready: " + isPlacementReady);
+    }
+}
